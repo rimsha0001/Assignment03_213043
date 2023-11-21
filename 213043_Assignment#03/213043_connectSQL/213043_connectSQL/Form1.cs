4146516fb085664bd5385ba2e2af493b7668fe78
Rimsha <merimsha8@gmail.com>
2023-11-22 01:31:29 +0500

    first commit

---
+++ 213043_Assignment#03/213043_connectSQL/213043_connectSQL/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Runtime.Remoting.Messaging;
+
+namespace _213043_connectSQL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string query;
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-46PNOSO\\SQLEXPRESS;Initial Catalog=shop;Integrated Security=True");
+            con.Open();
+            //MessageBox.Show("connection open");
+            SqlCommand cm = new SqlCommand();
+            //    cm.CommandText = "Delete FROM stationay where CustName = 'pen'";
+            //cm.Connection = con;
+            //int inRow = cm.ExecuteNonQuery();
+            //con.Close();
+            //query = "Insert into stationary(ID,CustName,UnitPrice,QuantityInStock) VALUES(4,'Clipboard',2,5)";
+            //SqlCommand cm1 = new SqlCommand(query, con);
+            cm.CommandText = "Insert INTO stationay(ID,CustName,UnitPrice,QuantityInStock) VALUES(4,'Clipboard'','2','5')";
+            cm.Connection = con;
+            cm.ExecuteNonQuery();
+            con.Close();
+            //Console.Write("Total row Insert" + totalRowAffected + " ");
+
+            //cm.CommandText = "select * from stationay";
+            //cm.CommandText = "Delete FROM stationay where CustName = 'pen'";
+            /*int totalRowAffected =*/
+            //cm.executenonquery();
+            //.Write("Total rows Deleted = " +)
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
